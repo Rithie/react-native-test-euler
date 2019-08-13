fcab24b297993a3a8dea381dfb0dc012a597e61f
Euler Alvarenga <euler.sistemas@gmail.com>
2019-08-13 11:28:03 -0300

    adiciona iphone-x helper e estilos para a tela de login

--- TesteEuler/src/pages/Login/index.js
+++ TesteEuler/src/pages/Login/index.js
@@ -1,112 +1,278 @@
 import React, { Component } from 'react';
 import {
-  TouchableOpacity,
-  View,
-  Text,
-  KeyboardAvoidingView, TextInput, Image,
+  StyleSheet, View, Text, KeyboardAvoidingView, TextInput, TouchableOpacity,
 } from 'react-native';
 
 import { connect } from 'react-redux';
 
-// import { Creators as AuthActions } from '../../store/reducers/auth';
+import { Creators as AuthActions } from '~/store/ducks/auth';
 
-import { Row } from '~/styles/global';
+import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 import {
   Label,
-  Input,
-  SimpleButtonLabel,
   SimpleLabel,
+  SolidButton,
   Container,
-  ErrorMessage,
+  SimpleButtonLabel,
   InputContainer,
+  OutlineButton,
+  Header,
+  Row,
 } from './styles';
 
-import { colors } from '~/styles/colors';
+
 
 class Login extends Component {
   state = {
-    email: '',
+    usuario: '',
     password: '',
-    errors: [],
-    emailErrors: '',
-    passwordErrors: '',
-    showEmailError: false,
-    showPasswordError: false,
-    securePassword2: true,
+    securePassword: true,
     visible: false,
+    showUsuarioError: false,
+    showPasswordError: false,
+    passwordErrors: '',
+    emailErrors: '',
+  };
+
+  revealPassword = () => {
+    const { securePassword } = this.state;
+    this.setState({
+      securePassword: !securePassword,
+    });
+  };
+
+  onChangePassword = (password) => {
+    this.setState({
+      password,
+    });
+  };
+
+  onChangeUsuario = (usuario) => {
+    this.setState({
+      usuario,
+    });
+  };
+
+  signIn = () => {
+    const { usuario, password } = this.state;
+    const { logInRequest } = this.props;
+
+    if (!usuario) {
+      this.setState({
+        showusuarioError: true,
+        usuarioErrors: 'This field is required.',
+      });
+      return false;
+    }
+    this.setState({
+      showusuarioError: false,
+      usuarioErrors: '',
+    });
+
+    if (!password) {
+      this.setState({
+        showPasswordError: true,
+        passwordErrors: 'This field is required.',
+      });
+      return false;
+    }
+    this.setState({
+      showPasswordError: false,
+      passwordErrors: '',
+    });
+
+    const { navigate } = this.props.navigation;
+
+    // navigate('App');
+    console.tron.log(this.state);
+
+    logInRequest(usuario, password);
   };
 
 
   render() {
+    const { securePassword, showUsuarioError } = this.state;
     return (
-      <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1 }}>
-        <Container
-          keyboardShouldPersistTaps="handled"
-          contentContainerStyle={{
-            flex: 1,
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            margin: 0,
-            paddingHorizontal: 16,
-            paddingVertical: 10,
-          }}
-        >
-          <Row marginTop={20} />
-          <View
-            style={{
-              width: '100%',
+      <LinearGradient colors={['#009ca0', '#0087a3', '#006da5']} style={{ flex: 1 }}>
+        <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1 }}>
+          <Container
+            keyboardShouldPersistTaps="handled"
+            contentContainerStyle={{
+              flex: 1,
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              margin: 0,
+              paddingHorizontal: 16,
+              paddingVertical: 10,
             }}
           >
-            <Label>Sign in</Label>
-            <Input
-              onChangeText={() => {}}
-              placeholder="Usuário"
-              autoCompleteType="email"
-              textContentType="emailAddress"
-              autoCorrect={false}
-              spellCheck={false}
-              autoCapitalize="none"
-              returnKeyType="next"
-              onSubmitEditing={() => {
-                this.secondTextInput.focus();
-              }}
-              blurOnSubmit={false}
-              // error={showEmailError}
-            />
-            <InputContainer>
-              <TextInput
-                style={{ width: '90%', padding: 10 }}
-                ref={(input) => {
-                  this.secondTextInput = input;
+            <Header>
+              <Icon
+                style={{
+                  margin: 5,
+                  opacity: 0.8,
                 }}
-                // onChangeText={}
-                autoCompleteType="password"
-                textContentType="password"
-                secureTextEntry
-                placeholder="Senha"
-                autoCapitalize="none"
-                returnKeyType="done"
-                onSubmitEditing={() => {
+                name="chevron-left"
+                size={25}
+                color="#fff"
+                solid
+              />
+              <View />
+                <FontAwesome
+                  style={{
+                    margin: 5,
+                    opacity: 0.8,
+                  }}
+                  name="question-circle-o"
+                  size={25}
+                  color="#fff"
+                  solid
+                />
+            </Header>
+            <View
+              style={{
+                width: '100%',
+              }}
+            >
+              <Label>Informe o usuário e a senha encaminhados pela escola</Label>
+              <Row>
+              <InputContainer borderRadiusTop>
+                <Icon
+                  style={{
+                    margin: 5,
+                    opacity: 0.8,
+                  }}
+                  name="user"
+                  size={16}
+                  color="#f9f9f9"
+                  solid
+                />
+                <TextInput
+                  style={{ width: '90%', padding: 10, color: '#fff' }}
+                  ref={(input) => {
+                    this.secondTextInput = input;
+                  }}
+                  onChangeText={this.onChangeUsuario}
+                  placeholder="Usuário"
+                  placeholderTextColor="#fff"
+                  autoCompleteType="email"
+                  textContentType="emailAddress"
+                  autoCorrect={false}
+                  spellCheck={false}
+                  autoCapitalize="none"
+                  returnKeyType="next"
+                  onSubmitEditing={() => {
+                    this.secondTextInput.focus();
+                  }}
+                  blurOnSubmit={false}
+                  error={showUsuarioError}
+                />
+              </InputContainer>
+              <InputContainer borderRadiusBottom noBorder>
+                <Icon
+                  style={{
+                    margin: 5,
+                    opacity: 0.8,
+                  }}
+                  name="key"
+                  size={16}
+                  color="#f9f9f9"
+                  solid
+                />
+                <TextInput
+                  style={{ width: '80%', padding: 10, color: '#fff' }}
+                  ref={(input) => {
+                    this.secondTextInput = input;
+                  }}
+                  onChangeText={this.onChangePassword}
+                  autoCompleteType="password"
+                  textContentType="password"
+                  secureTextEntry={securePassword}
+                  placeholder="Senha"
+                  placeholderTextColor="#fff"
+                  autoCapitalize="none"
+                  returnKeyType="done"
+                  onSubmitEditing={() => {
+                    this.signIn();
+                  }}
+                  blurOnSubmit={false}
+                />
+                <TouchableOpacity
+                onPress={() => {
+                  this.revealPassword();
+                }}
+              >
+                {securePassword ? (
+                  <FontAwesome
+                  style={{
+                    margin: 5,
+                    opacity: 0.8,
+                  }}
+                  name="eye"
+                  size={16}
+                  color="#f9f9f9"
+                  solid
+                  />
+                ) : (
+                  <FontAwesome
+                  style={{
+                    margin: 5,
+                    opacity: 0.8,
+                  }}
+                  name="eye-slash"
+                  size={16}
+                  color="#f9f9f9"
+                  solid
+                  />
+                )}
+              </TouchableOpacity>
+              </InputContainer>
+              </Row>
+              <SolidButton
+                onPress={() => {
                   this.signIn();
                 }}
-                blurOnSubmit={false}
-              />
-              </InputContainer>
+              >
+                <SimpleLabel color="#0087a3">Entrar</SimpleLabel>
+              </SolidButton>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 10 }}>
+                <TouchableOpacity onPress={() => {}}>
+                  <SimpleButtonLabel>Esqueci a minha senha</SimpleButtonLabel>
+                </TouchableOpacity>
+                <View style={{ borderWidth: StyleSheet.hairlineWidth, borderColor: 'white' }} />
+                <TouchableOpacity onPress={() => {}}>
+                  <SimpleButtonLabel>Politica de Privacidade</SimpleButtonLabel>
+                </TouchableOpacity>
+              </View>
             </View>
-            <Row />
-        </Container>
-      </KeyboardAvoidingView>
+
+            <OutlineButton>
+              <FontAwesome
+                style={{
+                  margin: 5,
+                }}
+                name="mobile-phone"
+                size={25}
+                color="#f9f9f9"
+                solid
+              />
+              <SimpleLabel color="#fff">Entrar com celular</SimpleLabel>
+              <View />
+            </OutlineButton>
+          </Container>
+        </KeyboardAvoidingView>
+      </LinearGradient>
     );
   }
 }
 
-const mapStateToProps = state => ({
-
-});
+const mapStateToProps = state => ({});
 
 const mapDispatchToProps = dispatch => ({
-
+  logInRequest: (login, password) => dispatch(AuthActions.logInRequest(login, password))
 });
 
 export default connect(
--- TesteEuler/src/pages/Login/styles.js
+++ TesteEuler/src/pages/Login/styles.js
@@ -1,17 +1,18 @@
 import styled from 'styled-components/native';
-import { Dimensions } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { ifIphoneX } from 'react-native-iphone-x-helper';
 import { colors } from '~/styles/colors';
 
 export const Container = styled.ScrollView``;
 
 export const Label = styled.Text`
-  font-size: 28px;
-  color: #f90;
-  font-weight: bold;
-  align-self: flex-start;
-  margin: 0px 0px 25px 0px;
+  font-size: 18px;
+  color: #fff;
 `;
 
+export const Row = styled.View`
+  margin: 20px 0 10px 0;
+`;
 export const ErrorMessage = styled.Text`
   font-size: 13px;
   color: red;
@@ -19,45 +20,76 @@ export const ErrorMessage = styled.Text`
   align-self: center;
 `;
 
-export const TextLabel = styled.Text`
-  font-size: 16;
-  color: #666;
-  font-weight: 400;
-  text-align: center;
-  padding: 20px 0px;
-`;
+export const Header = styled.View(props => ({
+  width: '100%',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  ...ifIphoneX(
+    {
+      paddingTop: 50,
+      height: 110,
+    },
+    {
+      paddingTop: Platform.OS === 'ios' ? 20 : 0,
+      height: 60,
+    },
+  ),
+}));
 
 export const SimpleLabel = styled.Text(props => ({
   fontSize: 14,
-  color: '#666',
-  fontWeight: props.bold ? 'bold' : 'none',
+  color: props.color ? props.color : '#fff',
   justifyContent: 'center',
   paddingHorizontal: props.paddingHorizontal || 0,
   paddingVertical: props.paddingVertical || 0,
 }));
 
-export const Input = styled.TextInput(props => ({
-  borderWidth: 1,
-  borderColor: props.error ? 'red' : 'grey',
-  padding: 10,
-  marginTop: 24,
-}));
-
 export const InputContainer = styled.View(props => ({
   borderWidth: 1,
-  borderColor: props.error ? 'red' : 'grey',
+  borderColor: props.error ? 'red' : 'white',
   padding: 0,
   flexDirection: 'row',
   alignItems: 'center',
   justifyContent: 'space-between',
+  borderWidth: StyleSheet.hairlineWidth,
+  borderTopWidth: props.noBorder ? 0 : StyleSheet.hairlineWidth,
+  borderTopLeftRadius: props.borderRadiusTop ? 4 : 0,
+  borderTopRightRadius: props.borderRadiusTop ? 4 : 0,
+  borderBottomLeftRadius: props.borderRadiusBottom ? 4 : 0,
+  borderBottomRightRadius: props.borderRadiusBottom ? 4 : 0,
+
 }));
 
 export const SimpleButtonLabel = styled.Text(props => ({
   fontSize: props.fontSize || 12,
-  color: 'blue',
+  color: 'white',
   marginTop: props.marginTop || 0,
   marginBottom: props.marginTop || 0,
   textDecorationLine: props.underline ? 'underline' : 'none',
   paddingHorizontal: props.paddingHorizontal || 0,
+}));
 
+export const SolidButton = styled.TouchableOpacity(props => ({
+  borderWidth: 1,
+  borderColor: 'white',
+  marginVertical: 10,
+  paddingVertical: 10,
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  borderWidth: StyleSheet.hairlineWidth,
+  borderRadius: 4,
+  backgroundColor: props.color ? props.color: '#fff',
+}));
+export const OutlineButton = styled.TouchableOpacity(props => ({
+  borderWidth: 1,
+  borderColor: 'white',
+  marginBottom: 30,
+  paddingVertical: 2,
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  borderWidth: StyleSheet.hairlineWidth,
+  borderRadius: 4,
+  width: '100%',
 }));
